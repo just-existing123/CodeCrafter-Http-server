c31df97e9cafb23f44c808d809f12de7518de667
KRISH <krishsood.24@gmail.com>
2025-11-19 01:04:37 +0530

    fix test

--- app/main.py
+++ app/main.py
@@ -8,7 +8,12 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection , address = server_socket.accept() # wait for client
+
+    #server_socket.accept() waits for the client and when the client joins the http server it returns the client IP and a TCP connection pipe to the client for information transport 
+    response = b"HTTP/1.1 200 OK\r\n\r\n"
+    connection.sendall(response)
+    server_socket.accept()
 
 
 if __name__ == "__main__":
