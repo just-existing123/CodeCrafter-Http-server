32c5263de0e8e9b09dccc3f1caf07b0cfcdfed2a
KRISH <krishsood.24@gmail.com>
2025-11-20 01:26:21 +0530

    windows tester added

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    # server_socket = socket.create_server(("localhost", 4221))
     # wait for client
 
     #server_socket.accept() waits for the client and when the client joins the http server it returns the client IP and a TCP connection pipe to the client for information transport 
